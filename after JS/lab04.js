93db496d978ffcdb2ff639fdeb147cfbad5409c7
Ahmedzatar <ahmed.zatar97@gmail.com>
2021-02-24 15:37:34 +0200

    submission lab06a

--- after JS/lab04.js
+++ after JS/lab04.js
@@ -1,6 +1,6 @@
+
 confirm("Welcome To The Art House");
 var name=prompt("What is your Name ?")
-var age=prompt("how old are you ?")
 var a=prompt("Do you like paint ?");
 var no="no";
 var yes="yes";
@@ -12,9 +12,31 @@ if (a.toLowerCase() === no){
 }else{ alert("wrong input");}
 
 var b=document.getElementById('username');
-b.innerText='Name : '+name.toUpperCase()
+b.innerText='WELCOME '+name.toUpperCase();
 
-var c=document.getElementById('userage');
-c.innerText='Age : '+age.toUpperCase()
+function avgOfRating(r1,r2,r3){
+  
+    var sum= parseInt(r1)+parseInt(r2)+parseInt(r3);
+    var avg=sum/3;
+    
+    var c=document.getElementById('rating');
+    c.innerText='Your Rating Us '+ avg+'/5'; 
+}
 
+var counter=0;
 
+while (counter<3){
+if (counter==0){
+    var r1 =prompt("How do you rate the drawings on our site from 5");
+
+}else if (counter==1){
+   var r2 =prompt("How would you rate us for choosing our site painter out of 5");
+       
+}else {
+   var r3 =prompt("What is your rating for website design from 5");
+}
+counter+=1;
+
+}
+
+avgOfRating(r1,r2,r3);
